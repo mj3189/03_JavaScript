e49f5d4b9af9d6fa6a14dba43efa242d675d31ea
DESKTOP-Q6S4J5T\user1 <mj3189@naver.com>
2024-01-22 17:23:09 +0900

    20240122_ 스탑워치

---
+++ js/23_스톱워치.js
@@ -0,0 +1,89 @@
+const display = document.querySelector("#display");
+const list = document.querySelectorAll("#display > span");
+const startBtn = document.querySelector("#startBtn");
+const recordBtn = document.querySelector("#recordBtn");
+const resetBtn = document.querySelector("#resetBtn");
+const recordContainer = document.querySelector("#recordContainer");
+
+
+let count = 0;  // 1/100 초마다 1씩 증가한 값을 저장할 변수
+
+// 1/100 초자리 : count % 100
+// 1초          : count / 100 % 60
+// 1분          : count / 100 / 60
+
+
+let currInterval;  // 인터벌 저장 변수
+
+/* START 버튼 클릭 시 10ms 마다 display 시간을 증가 */
+
+startBtn.addEventListener("click", e => {
+
+    currInterval = window.setInterval( ()=>{
+        count++;  // 카운트를 1증가
+
+        output();  // 화면에 시간 출력
+
+        // document.querySelectorAll("#display > span")[2].innerText = count;
+
+    }, 10);   // 10ms
+
+    e.target.disabled = true;  // 임시
+
+});
+
+
+
+/* 시계 출력 함수 */
+function output(){
+
+    let minute = Math.floor(count / 100 / 60);
+    let second = Math.floor(count / 100 % 60);
+    let ms     = count % 100;
+
+    // 0붙여서 문자열로 변환해서 반환
+    minute = attachZero(minute);
+    second = attachZero(second);
+    ms = attachZero(ms);
+
+    //console.log(minute, second, ms);
+
+
+    //minute의 값과 화면에 출력된 분이 다를 경우 
+    if(list[0].innerText != minute) list[0].innerText = minute;
+
+
+    //second의 값과 화면에 출력된 초가 다를 경우
+    if(list[1].innerText != second) list[1].innerText = second;
+
+    list[2].innerText = ms;
+
+}
+
+
+/* 한 자리 숫자인 경우 앞에 0 붙여서 반환하는 함수 */
+function attachZero(num){
+
+    if(num < 10){ // 한 자리 숫자인 경우
+      return "0" + num;
+    }
+    return "" + num;
+  }
+
+
+
+/* RESET 버튼이 클릭 되었을 때 */
+resetBtn.addEventListener("click", ()=>{
+
+    //currinterval 제거
+    window.clearInterval(currInterval);
+
+    // count, 출력된 시간 모두 0으로
+    count = 0;
+    output();
+
+
+    // start 버튼 다시 활성화
+    startBtn.disabled = false;
+
+});
